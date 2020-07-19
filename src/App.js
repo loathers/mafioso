109bc75f1f98abba29c4d834646f48543f3957b4
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-07-19 16:12:14 -0700

    implement data parsing in logStore.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+
+import logStore from 'store/logStore';
+
 import UploadComponent from 'components/UploadComponent';
 
 import './App.css';
+
+console.log('calling parse')
+logStore.parse();
 
 function App() {
   return (
--- src/store/logStore.js
+++ src/store/logStore.js
@@ -1,22 +1,80 @@
-class LogStore {
-  constructor() {
-    this.srcFile = undefined;
-  }
+const sampleLine1 = '0000\tDay\tTurn\tLocation\tEncounter\tFamiliar\tSpecial\tItems\tEffects\tMus\tMyst\tMox\tMeat';
+const sampleLine2 = '0003\t1\t1\tBoxingDaycare\tEntertheBoxing\tDaycare\tLeft-Hand Man\tBrutal brogues|sharkfin gumbo|\t0\t0\t0\t0'
+const sampleLog = `${sampleLine1}\n${sampleLine2}`;
 
-  get hasFile() {
-    return this.srcFile !== undefined;
-  }
+class LogLine {
+  /** @default */
+  constructor(lineStr) {
+    /** @type {String} */
+    this.srcLine = lineStr;
+    /** @type {Object} */
+    this.parsedData = undefined;
 
-  handleUpload(file) {
-    this.srcFile = file;
+    if (this.hasSrc) {
+      this.parse();
+    }
+  }
+  /**
+   * @type {Boolean}
+   */
+  get hasSrc() {
+    return this.srcLine !== undefined;
   }
 
   parse() {
-    if (!this.hasFile) {
+    if (!this.hasSrc) {
       return;
     }
 
-    
+    const lineSplit = this.srcLine.split('\t');
+    const newData = {
+      actionId: lineSplit[0],
+      day: lineSplit[1],
+      turn: lineSplit[2],
+      location: lineSplit[3],
+      encounter: lineSplit[4],
+      familiar: lineSplit[5],
+      special: lineSplit[6],
+      items: lineSplit[7],
+      effects: lineSplit[8],
+      mus: lineSplit[9],
+      myst: lineSplit[10],
+      mox: lineSplit[11],
+      meat: lineSplit[12],
+    };
+
+    this.parsedData = newData;
+    console.log('#newData', newData)
+  }
+}
+
+class LogStore {
+  /** @default */
+  constructor() {
+    /** @type {String} */
+    this.srcLog = sampleLog;
+    /** @type {Array<LogLine>} */
+    this.parsedData = undefined;
+  }
+  /**
+   * @type {Boolean}
+   */
+  get hasSrc() {
+    return this.srcLog !== undefined;
+  }
+
+  handleUpload(file) {
+    this.srcLog = file;
+  }
+
+  parse() {
+    if (!this.hasSrc) {
+      return;
+    }
+
+    const logSplit = this.srcLog.split('\n');
+    const logDataArray = logSplit.map((lineStr) => new LogLine(lineStr));
+    this.parsedData = logDataArray;
   }
 }
 
