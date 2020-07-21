4f5fc5ba66b6e07cecc0e3b174b73b43155c818e
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-07-20 22:00:51 -0700

    EntryDisplayContainer.js - support new line for entry text

--- src/components/EntryDisplayContainer.js
+++ src/components/EntryDisplayContainer.js
@@ -18,7 +18,7 @@ export default function EntryDisplayContainer(props) {
     <div className={'flex-row aitems-start adjacent-mar-t-2 bg-second pad-2 borradius-2 ' + className}>
       {/* entry adventure num */}
       <div 
-        className='color-gray fontsize-1 aself-start flex-none adjacent-mar-l-2'
+        className='flex-row jcontent-end color-gray fontsize-1 aself-start flex-none adjacent-mar-l-2'
         style={{width: 30}}>
         {adventureNum === -1 ? '' : adventureNum}
       </div>
@@ -31,7 +31,7 @@ export default function EntryDisplayContainer(props) {
           height: 25,
         }} />
 
-      <div className='flex-auto adjacent-mar-l-2'>{props.children}</div>
+      <div className='flex-auto adjacent-mar-l-2 whitespace-pre-wrap'>{props.children}</div>
     </div>
   )
 }
