3aea2181ec83656e039b71893e0145e14e42491f
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-07-21 10:57:31 -0700

    VisualizerSection.js - show original entryString when selected

--- src/sections/VisualizerSection.js
+++ src/sections/VisualizerSection.js
@@ -88,44 +88,56 @@ export default observer(
 function VisualizerSection(props) {
   const {logData = []} = props;
 
+  // list state
   const [selectedEntry, setSelectedEntry] = React.useState();
-
-  // focus hook
+  const [previousEntry, setPreviousEntry] = React.useState();
   const [focusedEntry, setFocusedEntry] = React.useState();
-  // const [elementPosition, setFocusPosition] = React.useState({x: 10, y: 210});
-  // const [previousEntry, setPreviousEntry] = React.useState();
-  // const [previousPosition, setPreviousPosition] = React.useState({x: 10, y: 210});
 
-  const onEnterItem = (evt, newEntry) => {
-    // const boundingClientRect = evt.currentTarget.getBoundingClientRect();
-    // const yOffset = (newEntry.entryString.length * -0.6);
-    // const yRandom = Math.random() * 10 - 5;
+  // tooltip state 
+  const [elementPosition, setFocusPosition] = React.useState({x: 10, y: 10});
+  const [previousPosition, setPreviousPosition] = React.useState({x: 10, y: 10});
 
-    // const xPosition = evt.clientX - 250;
-    // setFocusPosition({x: xPosition, y: boundingClientRect.y + yOffset + yRandom});
+  const oSelectItem = (evt, newEntry) => {
+    if (previousEntry && selectedEntry !== undefined && previousEntry.id === newEntry.id) {
+      setSelectedEntry(undefined);
+      return;
+    }
+
+    const boundingClientRect = evt.currentTarget.getBoundingClientRect();
+
+    const xPosition = boundingClientRect.x;
+    const yOffset = (newEntry.entryString.length * -0.8);
+    const yPosition = boundingClientRect.y + yOffset;
+    setFocusPosition({x: xPosition, y: yPosition});
+
+    setPreviousPosition(elementPosition);
+    setPreviousEntry(focusedEntry);
+    setSelectedEntry(newEntry);
+  };
+
+  const onMouseEnterItem = (evt, newEntry) => {
     setFocusedEntry(newEntry);
   };
 
-  const onLeaveItem = () => {
-    // setPreviousPosition(elementPosition);
-    // setPreviousEntry(focusedEntry);
+  const onMouseLeaveItem = () => {
     setFocusedEntry(undefined);
   };
 
   const hasFocusedEntry = focusedEntry !== undefined;
   const hasSelectedEntry = selectedEntry !== undefined;
-  // const positionToUse = hasFocusedEntry ? elementPosition : previousPosition;
-  // const detailsStyle = {
-  //   transform: `translate(${positionToUse.x}px, ${positionToUse.y}px)`,
-  // };
+
+  const positionToUse = hasSelectedEntry ? elementPosition : previousPosition;
+  const detailsStyle = {
+    transform: `translate(${positionToUse.x}px, ${positionToUse.y}px)`,
+  };
 
   return (
     <div className='flex-col adjacent-mar-t-5'>      
       { logData.map((logEntry, idx) => (
         <VisualizerLine 
-          onClick={() => setSelectedEntry(logEntry)}
-          onMouseEnter={(evt) => onEnterItem(evt, logEntry)}
-          onMouseLeave={onLeaveItem}
+          onClick={(evt) => oSelectItem(evt, logEntry)}
+          onMouseEnter={(evt) => onMouseEnterItem(evt, logEntry)}
+          onMouseLeave={onMouseLeaveItem}
 
           isSelected={hasSelectedEntry && logEntry.id === selectedEntry.id}
           isFocused={hasFocusedEntry && logEntry.id === focusedEntry.id}
@@ -134,21 +146,20 @@ function VisualizerSection(props) {
           key={`VisualizerLine-${idx}-key`}/>
       ))}
 
-      {/*<div
+      <div
         className='pad-4 borradius-2 bg-grayest color-white whitespace-pre-wrap flex-col'
         style={{
           boxShadow: '0px 2px 1px 1px #1e3654',
-          pointerEvents: 'none',
           position: 'fixed',
           top: 0,
-          left: 100,
+          left: 30,
           transition: 'transform 500ms, opacity 300ms',
           width: 300,
-          opacity: hasFocusedEntry ? 1 : 0,
+          opacity: hasSelectedEntry ? 1 : 0,
           ...detailsStyle,
         }}>
-        {focusedEntry && focusedEntry.entryString}
-      </div>*/}
+        {(selectedEntry && selectedEntry.entryString) || (previousEntry && previousEntry.entryString)}
+      </div>
     </div>
   )
 })
