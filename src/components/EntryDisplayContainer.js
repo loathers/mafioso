9f88a36c4a1108c82447896e6df3ce268b543ad6
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-07-20 23:04:46 -0700

    EntryDisplayContainer.js - show "free" when adv did not cost a turn

--- src/components/EntryDisplayContainer.js
+++ src/components/EntryDisplayContainer.js
@@ -9,18 +9,31 @@ export default function EntryDisplayContainer(props) {
   const {
     className,
     IconComponent = UncertaintySVG,
-    logEntry
+    logEntry,
   } = props;
 
-  const adventureNum = logEntry.data.adventureNum;
+  const {data} = logEntry;
+  const {
+    adventureNum,
+    isFreeAdv,
+  } = data;
 
   return (
     <div className={'flex-row aitems-start adjacent-mar-t-2 bg-second pad-2 borradius-2 ' + className}>
-      {/* entry adventure num */}
-      <div 
-        className='flex-row jcontent-end color-gray fontsize-1 aself-start flex-none adjacent-mar-l-2'
-        style={{width: 30}}>
-        {adventureNum === -1 ? '' : adventureNum}
+      <div
+        className='flex-col flex-none adjacent-mar-l-2'>      
+        {/* entry adventure num */}
+        <div 
+          className='talign-right color-gray fontsize-1 aself-start adjacent-mar-t-3'
+          style={{width: 30}}>
+          {adventureNum === -1 ? '' : adventureNum}
+        </div>
+
+        { isFreeAdv &&
+          <div className='talign-right color-gray fontsize-1 adjacent-mar-t-3'>
+            free
+          </div>
+        }
       </div>
 
       {/* entry icon */}
