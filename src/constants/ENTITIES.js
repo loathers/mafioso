62bf50d69ba80fd1f0727fd6857886932b3a38f7
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-07-31 11:12:49 -0700

    create BANISHERS, ENTITIES, entityType constants

---
+++ src/constants/ENTITIES.js
@@ -0,0 +1,26 @@
+import ENTITY_TYPE from 'constants/entityType';
+
+export const LOUDER_THAN_BOMB = {
+  name: 'Louder Than Bomb',
+  entityType: ENTITY_TYPE.COMBAT_ITEM,
+}
+export const HAROLDS_BELL = {
+  name: 'Harold\'s bell',
+  entityType: ENTITY_TYPE.COMBAT_ITEM,
+}
+export const TRYPTOPHAN_DART = {
+  name: 'tryptophan dart',
+  entityType: ENTITY_TYPE.COMBAT_ITEM,
+}
+export const REFLEX_HAMMER = {
+  name: 'Reflex Hammer',
+  entityType: ENTITY_TYPE.EQUIPMENT,
+}
+export const USE_THE_FORCE = {
+  name: 'Use the Force',
+  entityType: ENTITY_TYPE.EQUIPMENT,
+}
+export const BATTER_UP = {
+  name: 'Batter Up',
+  entityType: ENTITY_TYPE.COMBAT_SKILL,
+}
