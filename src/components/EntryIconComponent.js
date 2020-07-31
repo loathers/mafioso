1c3043994b183a78e9e80bd54aea14487629c6bc
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-07-30 22:02:22 -0700

    add condor-emblem for Ascension icon

--- src/components/EntryIconComponent.js
+++ src/components/EntryIconComponent.js
@@ -6,6 +6,7 @@ import { ReactComponent as AnvilImpactSVG } from 'images/anvil-impact.svg';
 import { ReactComponent as BattleGearSVG } from 'images/battle-gear.svg';
 import { ReactComponent as CatSVG } from 'images/cat.svg';
 import { ReactComponent as CrossedSwordsSVG } from 'images/crossed-swords.svg';
+import { ReactComponent as CondorEmblemSVG } from 'images/condor-emblem.svg';
 import { ReactComponent as DigDugSVG } from 'images/dig-dug.svg';
 import { ReactComponent as InfoSVG } from 'images/info.svg';
 import { ReactComponent as ShopSVG } from 'images/shop.svg';
@@ -26,6 +27,12 @@ import { ReactComponent as UncertaintySVG } from 'images/uncertainty.svg';
  */
 function getEntryIcon(entryType) {
   switch(entryType) {
+    case ENTRY_TYPE.SNAPSHOT.DAY_INFO:
+    case ENTRY_TYPE.SNAPSHOT.SKILL_BREAKDOWN:
+    case ENTRY_TYPE.SNAPSHOT.ASCENSION_INFO:
+    case ENTRY_TYPE.SNAPSHOT.VALHALLA:
+      return CondorEmblemSVG;
+
     case ENTRY_TYPE.PVP:
     case ENTRY_TYPE.IOTM.GOD_LOBSTER.COMBAT:
     case ENTRY_TYPE.ENCOUNTER.COMBAT:
@@ -45,9 +52,6 @@ function getEntryIcon(entryType) {
     case ENTRY_TYPE.IOTM.SONGBOOM_BOOMBOX:
     case ENTRY_TYPE.IOTM.VOTING_BOOTH:
     case ENTRY_TYPE.CLAN_VISIT:
-    case ENTRY_TYPE.SNAPSHOT.ASCENSION_INFO:
-    case ENTRY_TYPE.SNAPSHOT.DAY_INFO:
-    case ENTRY_TYPE.SNAPSHOT.SKILL_BREAKDOWN:
       return InfoSVG;
 
     case ENTRY_TYPE.IOTM.GARBAGE_TOTE:
