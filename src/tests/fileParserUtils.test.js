b076e2a9617ae7bbd2f87d0d786648aaecd72ef7
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-08-06 00:07:01 -0700

    implement "getNameFromSessionFile()" and unit test

--- src/tests/fileParserUtils.test.js
+++ src/tests/fileParserUtils.test.js
@@ -7,15 +7,15 @@ const mockFileB = new File([], 'dextrial_20200731.txt');
 const mockFileC = new File([], 'dextrial_20200730.txt');
 const mockFileD = new File([], 'c00lest_one_20200730.txt');
 
-test('fileParserUtils.getSessionDateFromFile(): returns correct date from filename', () => {
-  const sessionDate = fileParserUtils.getSessionDateFromFile(mockFileA);
+test('fileParserUtils.getDateFromSessionFile(): returns correct date from filename', () => {
+  const sessionDate = fileParserUtils.getDateFromSessionFile(mockFileA);
   expect(sessionDate.getFullYear()).toBe(2020);
   expect(sessionDate.getMonth()).toBe(8);
   expect(sessionDate.getDate()).toBe(1);
 });
 
-test('fileParserUtils.getSessionDateFromFile(): handles filename with a number', () => {
-  const sessionDate = fileParserUtils.getSessionDateFromFile(mockFileD);
+test('fileParserUtils.getDateFromSessionFile(): handles filename with a number', () => {
+  const sessionDate = fileParserUtils.getDateFromSessionFile(mockFileD);
   expect(sessionDate.getFullYear()).toBe(2020);
   expect(sessionDate.getMonth()).toBe(7);
   expect(sessionDate.getDate()).toBe(30);
@@ -26,4 +26,12 @@ test('fileParserUtils.sortBySessionDate(): handles sorting out of order sessions
   expect(sortedSessions[0].name).toBe('dextrial_20200730.txt');
   expect(sortedSessions[1].name).toBe('dextrial_20200731.txt');
   expect(sortedSessions[2].name).toBe('dextrial_20200801.txt');
+});
+
+test('fileParserUtils.getNameFromSessionFile(): returns string of name', () => {
+  const userName = fileParserUtils.getNameFromSessionFile(mockFileA);
+  expect(userName).toBe('dextrial');
+
+  const userName2 = fileParserUtils.getNameFromSessionFile(mockFileD);
+  expect(userName2).toBe('c00lest-one');
 });
--- src/utilities/fileParserUtils.js
+++ src/utilities/fileParserUtils.js
@@ -1,11 +1,10 @@
-
 /**
  * @params {Files|Array<File>} 
  */
 export function sortBySessionDate(files) {
   return Array.from(files).sort((fileA, fileB) => {
-    const sessionDateA = getSessionDateFromFile(fileA);
-    const sessionDateB = getSessionDateFromFile(fileB);
+    const sessionDateA = getDateFromSessionFile(fileA);
+    const sessionDateB = getDateFromSessionFile(fileB);
     return sessionDateA < sessionDateB ? -1 : 1;
   });
 }
@@ -13,11 +12,20 @@ export function sortBySessionDate(files) {
  * @param {File} file
  * @returns {Date}
  */
-export function getSessionDateFromFile(file) {
+export function getDateFromSessionFile(file) {
   const nameParts = file.name.split('_');
-  const sessionDate = nameParts[nameParts.length - 1];
+  const sessionDate = nameParts.pop();
   const year = sessionDate.slice(0, 4);
   const month = sessionDate.slice(4, 6);
   const day = sessionDate.slice(6, 8);
   return new Date(year, month, day);
+}
+/**
+ * @param {File} file
+ * @returns {String}
+ */
+export function getNameFromSessionFile(file) {
+  const nameParts = file.name.split('_');
+  nameParts.pop();
+  return nameParts.join('-');
 }
