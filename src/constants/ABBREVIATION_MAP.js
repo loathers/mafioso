6bbabd23eecaf7fb01f0464ea1f8e32a58113e0a
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-08-06 00:18:52 -0700

    ABBREVIATION_MAP.js - support all path names

--- src/constants/ABBREVIATION_MAP.js
+++ src/constants/ABBREVIATION_MAP.js
@@ -3,7 +3,7 @@
  */
 export function createAbbreviation(difficultyName = '', pathName = '') {
   const difficultyAbbr = DIFFICULTY_MAP[difficultyName.toLowerCase()];
-  const pathAbbr = PATH_MAP[pathName.replace(/ /g, '_').toLowerCase()];
+  const pathAbbr = PATH_MAP[pathName];
   return (`${difficultyAbbr}_${pathAbbr}`).toUpperCase();
 }
 /**
@@ -20,7 +20,39 @@ export const DIFFICULTY_MAP = {
  * @typedef {String} PathAbbr
  */
 export const PATH_MAP = {
-  standard: 'STD',
-  low_key_summer: 'LKS',
-  community_service: 'CS',
+  'Standard': 'STD',
+  'Low Key Summer': 'LKS',
+  'Path of the Plumber': 'PLUMB',
+  'Kingdom of Exploathing': 'EXPLO',
+  'Two Crazy Random Summer': '2CRS',
+  'Dark Gyffte': 'DG',
+  'Disguises Delimit': 'DD',
+  'G-Lover': 'GLOV',
+  'Pocket Familiars': 'POKEFAM',
+  'Live. Ascend. Repeat.': 'LAR',
+  'Gelatinous Noob': 'GN',
+  'Nuclear Autumn': 'NA',
+  'The Source': 'SRC',
+  'Avatar of West of Loathing': 'AOWOL',
+  'Community Service': 'CS',
+  'One Crazy Random Summer': '1CRS',
+  'Actually Ed the Undying': 'ED',
+  'Picky': 'PICKY',
+  'Heavy Rains': 'HR',
+  'Slow and Steady': 'S&S',
+  'Avatar of Sneaky Pete': 'PETE',
+  'Class Act II: A Class For Pigs': 'CAII',
+  'KOLHS': 'KOLHS',
+  'BIG!': 'BIG',
+  'Avatar of Jarlsberg': 'JARLS',
+  'Class Act': 'CA',
+  'Zombie Slayer': 'ZS',
+  'Bugbear Invasion': 'BI',
+  'Avatar of Boris': 'BORIS',
+  'Trendy': 'TREND',
+  'Way of the Surprising Fist': 'FIST',
+  'Bees Hate You': 'BEES',
+  'Oxygenarian': 'OXY',
+  'Boozetafarian': 'BOOZE',
+  'Teetotaler': 'TEET',
 };
