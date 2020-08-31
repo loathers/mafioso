9e3dd7e4c6b1269061cca271c2471a0161a42b69
aahvocado <dan.dan.makes.stuff@gmail.com>
2020-08-31 15:56:36 -0700

    create empty state for database list and button to view log

--- src/pages/DatabasePage.js
+++ src/pages/DatabasePage.js
@@ -4,6 +4,8 @@ import {observer} from 'mobx-react';
 import appStore from 'store/appStore';
 import databaseStore from 'store/databaseStore';
 
+import combineClassnames from 'utilities/combineClassnames';
+
 export default observer(
 function DatabasePage(props) {
 
@@ -14,25 +16,47 @@ function DatabasePage(props) {
     databaseStore.fetch();
   })
 
-  console.log('databaseStore.databaseList', databaseStore.databaseList);
+  const databaseList = databaseStore.databaseList;
 
   return (
     <div
       elementname='app-page-database'
       className='fontsize-4 flex-col'>
 
-      {databaseStore.databaseList.map((rowData, idx) => (
+      {databaseList.map((rowData, idx) => (
         <DatabaseRow
           key={`database-list-row-${idx}-key`}
           data={rowData} />
       ))}
 
+      {databaseList.length <= 0 &&
+        <div className='flex-row-center fontsize-6 color-white flex-auto adjacent-mar-t-5'>
+          Database unavailable
+        </div>
+      }
+
     </div>
   )
 })
-
+/** @returns {ReactComponent} */
 function DatabaseRow(props) {
   const {
+    data,
+  } = props;
+
+  return (
+    <div className='bor-1-second-darker borradius-2 bg-second pad-1 flex-row adjacent-mar-t-1'>
+      <button className='pad-2 pad-h-4 borradius-2 bg-second hover:bg-second-lighter adjacent-mar-l-4'>View</button>
+      <RowDisplay
+        data={data}
+        className='adjacent-mar-l-4'/>
+    </div>
+  )
+}
+/** @returns {ReactComponent} */
+function RowDisplay(props) {
+  const {
+    className,
     data: {
       charName,
       pathName,
@@ -43,7 +67,7 @@ function DatabaseRow(props) {
   } = props;
 
   return (
-    <div className='bor-1-second-darker borradius-2 bg-second hover:bg-second-lighter pad-2 flex-row adjacent-mar-t-1'>
+    <div className={combineClassnames('bor-1-second-darker borradius-2 bg-second hover:bg-second-lighter pad-2 flex-row adjacent-mar-t-1', className)}>
       <span className='f-bold adjacent-mar-l-2'>{charName}</span>
       <span className='adjacent-mar-l-2'>in</span>
       <span className='f-bold adjacent-mar-l-2'>{`${difficultyName} ${pathName}`}</span>
